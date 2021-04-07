4ef74593c3238dd66232f080bc0f0acf45bb9dff
SharmaRakeshPFE <63040045+SharmaRakeshPFE@users.noreply.github.com>
2021-04-07 18:41:13 +0530

    Update Export_Bacpac_MultipleServers_AllDB_STMicro.ps1

--- Export_Bacpac_MultipleServers_AllDB_STMicro.ps1
+++ Export_Bacpac_MultipleServers_AllDB_STMicro.ps1
@@ -1,5 +1,4 @@
-﻿##Script to take backup (bacpac) for multiple databases from multiple servers in their respective folders
-## Customer - STMicroelectronics 
+##Script to take backup (bacpac) for multiple databases from multiple servers in their respective folders
 ##Author - Rakesh Sharma
 ## Ver 1.0
 ##How to use##
@@ -7,36 +6,15 @@
 ##Create a CSV or Txt File containing list of Azure SQL Logical Servers
 ##Create database GET_AZURESQL_STATS
 ##Create a table to store the credential in database GET_AZURESQL_STATS
-##Code Reusability Support - Yes
-<#
-
-CREATE DATABASE GET_AZURE_STATS
-GO
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[TBL_CREDENTIAL](
-	[LOGICAL_SERVER] [varchar](400) NULL,
-	[USERNAME] [varchar](100) NULL,
-	[PWD] [varchar](200) MASKED WITH (FUNCTION = 'default()') NULL
-) ON [PRIMARY]
-GO
-
-#>
 
 cls ##Clearing Screen
-##Reading the Azure Logical Servers from list##
-##Create a text File with the list of servers on which backup has to be taken
-$SqlInstances= Get-Content -Path C:\STMicroelectronics\servers.txt
+##Reading the SQL Servers from list##
+$SqlInstances= Get-Content -Path C:\Infosys\SqlPackage\servers.txt
 
 ##Provide the name of the Local Instance to store the data##
 $LocalInstance ='MININT-B7K1QNM\SQL01'
 
-##Provide the credential to connect to local repository##
+##PRovide the credential to connect to local repository##
 $localUser='sa'
 $localPwd='sa'
 
@@ -47,8 +25,8 @@ $Repositorydb='GET_AZURESQL_STATS'
  foreach ($SqlInstance in $SqlInstances)  
   {
 
-    ##try
-      ##  {     
+    try
+        {     
             Write-Host 'connecting to Instance =' $SqlInstance
             Write-Host 'Reading Credentials to Connect to ' $SqlInstance
            
@@ -58,43 +36,63 @@ $Repositorydb='GET_AZURESQL_STATS'
             $username=$LocalCred.USERNAME
             $pwd=$LocalCred.pwd
 
-                               $Folder="C:\STMicroelectronics\SqlPackage\Files\" + $SqlInstance
+                               $Folder="C:\Infosys\SqlPackage\Files\" + $SqlInstance
                                $Folder=$Folder.Replace(".","_")
                                Write-Host "Creating Directories"
                                md $Folder -Force
                                Write-Host $Folder
                                Write-Host "Initiating BACPAC process for " $SqlInstances
                                 ##Pulling list of databases using above credentials -> Make sure that the credential have access to all databases##
-                                
-                                ##Important = In case you need bacpac for all databases change where name <> 'master'"
-                                ## Test this script by adding name of database##
-
-                                $databases=Invoke-Sqlcmd -Query "select name from [sys].databases where name = 'azsqldb'" -ServerInstance $SqlInstance -Username $username -Password $pwd -Database 'master' 
+                                $databases=Invoke-Sqlcmd -Query "select name from [sys].databases where name <> 'master'" -ServerInstance $SqlInstance -Username $username -Password $pwd -Database 'master' 
                                               foreach ($databaseName in $databases.name)
                                             {
+                                                  
                                                    $Path=   $Folder + "\" + $databaseName
-                                                   $ComandoOut="C:\STMicroelectronics\SqlPackage\Script\Export.bat " + $SqlInstance + " " + $databaseName + " " + $username + " " + $pwd + " " + $Path + ".bacpac" 
-                                                  $SqlQuery_1="INSERT INTO [TBL_BACPAC_LOCATIONS] (LSERVER,DATABASENAME,BACPAC_PATH,RESTORE_FLAG) VALUES (" + "'" + $SqlInstance + "'," + "'" +  $databaseName + "'," + "'" + $Path + "'," + "0" +")"
-                                                    write-host $SqlQuery_1
+                                                   $ComandoOut="C:\Infosys\SqlPackage\Script\Export.bat " + $SqlInstance + " " + $databaseName + " " + $username + " " + $pwd + " " + $Path + ".bacpac" 
+                                                   $STARTTIMESTAMP=Get-Date
+                                                   Write-Host $STARTTIMESTAMP
+
+                                                    $SqlQuery_1="INSERT INTO [TBL_BACPAC_LOCATIONS] (LSERVER,DATABASENAME,BACPAC_PATH,RESTORE_FLAG) VALUES (" + "'" + $SqlInstance + "'," + "'" +  $databaseName + "'," + "'" + $Path + "'," + "0" +")"
+                                                   $SqlQuery_2="INSERT INTO [TBL_BACPAC_TIMESTAMP] VALUES (" + "'" + $SqlInstance + "'," + "'" +  $databaseName + "'," + "'" + "BACKUP STARTED" + "'" + "," + "'"+ $STARTTIMESTAMP + "'" + "," + $DBsize.DbSizeInMB + ")" 
+                                                   $SQL_TSQL_Size="SELECT ((SUM(reserved_page_count) * 8192) / 1024 / 1024) AS DbSizeInMB FROM sys.dm_db_partition_stats"
+                                                                                                      
+                                                   write-host $SqlQuery_1
+                                                   write-host $SqlQuery_2
+                                                   
+
+                                                   ##Get Database Size##
+                                                   $DBsize=Invoke-Sqlcmd $SQL_TSQL_Size -ServerInstance $SqlInstance -Database $databaseName -Username $username -Password $pwd
+                                                                                                     
+                                                   Write-Host $DBsize.DbSizeInMB
+                                                   
                                                    Invoke-Sqlcmd $SqlQuery_1 -ServerInstance $LocalInstance -Username $localUser -Password $localPwd -Database $Repositorydb
+                                                   Invoke-Sqlcmd $SqlQuery_2 -ServerInstance $LocalInstance -Username $localUser -Password $localPwd -Database $Repositorydb
                                                    Write-Host "BACPAC PATH STORED IN DB"
                                                    Write-Host "####################################" 
                                                    Write-Host $ComandoOut
-                                                   #Intentionally Supressing the output for security reasons##
+                                                   
+                                                   
+                                                   
+                                                   ##Intentionally Supressing the output for security reasons##
                                                    Write-Host "Creating BACPAC for database " $databaseName
                                                    Invoke-Expression -Command $ComandoOut | Out-Null
+                                                   
+                                                   $ENDTIMESTAMP=Get-Date    
+                                                   Write-Host $ENDTIMESTAMP                                               
+                                                   $SqlQuery_3="INSERT INTO [TBL_BACPAC_TIMESTAMP] VALUES (" + "'" + $SqlInstance + "'," + "'" +  $databaseName + "'," + "'" + "BACKUP COMPLETED" + "'" + "," +"'"+ $ENDTIMESTAMP + "'" + "," + $DBsize.DbSizeInMB + ")" 
+                                                   Write-Host $SqlQuery_3
+                                                   Invoke-Sqlcmd $SqlQuery_3 -ServerInstance $LocalInstance -Username $localUser -Password $localPwd -Database $Repositorydb
                                                    $path=""
                                                    Write-Host "Bacpac completed for " $databaseName 
                                             }
 
-        ##}    
- 
- 
- ##catch
-  ##{
-    ##Write-Host -ForegroundColor DarkYellow "Error:"
-    ##Write-Host -ForegroundColor Magenta $Error[0].Exception
-  ##}
+        }    
+  
+ catch
+  {
+    Write-Host -ForegroundColor DarkYellow "Error:"
+    Write-Host -ForegroundColor Magenta $Error[0].Exception
+  }
  
  
  }
